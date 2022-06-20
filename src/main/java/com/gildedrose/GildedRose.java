e7c7c85c3fdb88b08ec62d41eb41f91dcf317282
MarvinElRhumAdorator <marvingilly@yahoo.fr>
2022-06-20 20:10:20 +0200

    first refacto

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,11 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
+    private Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,54 +13,66 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+            updateItem(items[i]);
+        }
+    }
+
+    private void updateItem(Item item) {
+        if (isNamed(item, SULFURAS_HAND_OF_RAGNAROS)) {
+            return;
+        }
+        updateSellIn(item);
+        updateQuality(item);
+    }
+
+    private void updateQuality(Item item) {
+        if (isNamed(item, AGED_BRIE)) {
+            increaseQuality(item, 1);
+        } else if (isNamed(item, BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            if (item.sellIn < 5) {
+                increaseQuality(item, 3);
+            } else if (item.sellIn < 10) {
+                increaseQuality(item, 2);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                increaseQuality(item, 1);
             }
+        } else {
+            decreaseQuality(item);
+        }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+        if (item.sellIn < 0) {
+            if (isNamed(item, AGED_BRIE)) {
+                increaseQuality(item, 1);
+            } else if (isNamed(item, BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                item.quality = 0;
+            } else {
+                decreaseQuality(item);
             }
+        }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private boolean isNamed(Item item, String agedBrie) {
+        return item.name.equals(agedBrie);
+    }
+
+    private void updateSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void increaseQuality(Item item, int increment) {
+        for (int i = 0; i < increment; i++) {
+            increaseQuality(item);
+        }
+    }
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
